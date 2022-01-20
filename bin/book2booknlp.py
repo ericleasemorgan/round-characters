3810e0e5b281051526fe0f856161e9edf98fcc60
Eric Lease Morgan <emorgan@nd.edu>
2022-01-20 10:11:50 -0500

    Added book2booknlp.py

---
+++ bin/book2booknlp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+# configure
+PARAMETERS = { "pipeline":"entity,quote,supersense,event,coref", "model":"big" }
+LANGUAGE   = 'en'
+
+# require
+from booknlp.booknlp import BookNLP
+import sys
+
+# sanity check
+if len( sys.argv ) != 4 : sys.exit( 'Usage: ' + sys.argv[ 0 ] + " <id> <file> <directory>\n" )
+
+# get input
+id        = sys.argv[ 1 ]
+file      = sys.argv[ 2 ]
+directory = sys.argv[ 3 ]
+
+# initialize
+booknlp = BookNLP( LANGUAGE, PARAMETERS )
+
+# do the work and done
+booknlp.process( file, directory, id )
+exit()
